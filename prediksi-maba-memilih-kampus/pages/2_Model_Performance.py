e58dfb675dbda2f5191b3dd5a77910b08ab96248
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 08:54:51 +0700

    Update 2_Model_Performance.py

--- prediksi-maba-memilih-kampus/pages/2_Model_Performance.py
+++ prediksi-maba-memilih-kampus/pages/2_Model_Performance.py
@@ -1,23 +1,15 @@
 import streamlit as st
-import pandas as pd
 import pickle
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-
-st.title("📈 Model Performance")
+import os
 
 @st.cache_resource
 def load_model():
-    with open("model.pkl", "rb") as f:
+    # Ambil folder utama proyek (naik 1 tingkat dari /pages)
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    model_path = os.path.join(base_dir, "model.pkl")
+    
+    # Buka model dari folder utama
+    with open(model_path, "rb") as f:
         return pickle.load(f)
 
 model = load_model()
-
-df = pd.read_csv("dataset.csv", sep=",")
-X = df[['Akreditasi', 'Uang_Kuliah', 'Fasilitas', 'Pelayanan', 'Lokasi']]
-y = df['Pilih_PTN']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-y_pred = model.predict(X_test)
-st.text("Classification Report:")
-st.text(classification_report(y_test, y_pred))
