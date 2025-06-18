fe7b34cba11af9b0183489a7df33edd59de6eade
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 08:51:29 +0700

    Update 1_Dashboard.py

--- prediksi-maba-memilih-kampus/pages/1_Dashboard.py
+++ prediksi-maba-memilih-kampus/pages/1_Dashboard.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 st.title("📊 Dashboard Data")
 
-df = pd.read_csv("dataset.csv", sep=",")
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+df = pd.read_csv(os.path.join(BASE_DIR, "dataset.csv"), sep=",")
+
 st.write("### Contoh Data")
 st.dataframe(df.head())
 
